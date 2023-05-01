91b1174d8efa5f78e93e4d8ee5ddc7c36718aa71
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-05-01 04:26:20 +0000

    few changes as per video

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
 
 # for the sake of changing - 1
-gcloud config set project kube-project-385314
-kubectl apply -n default -s $1 -f ds1.yaml --token=$2  
+kubectl apply -n default -f ds1.yaml
