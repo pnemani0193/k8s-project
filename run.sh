a3ea40568d2a2f1556aece5d7aba5e7adc363c39
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-05-01 01:51:42 +0000

    deleted get-credentials

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@
 
 echo $1 > token-file
 gcloud config set project kube-project-385314
-gcloud container clusters get-credentials apc-pn --region us-east1 --project kube-project-385314 --access-token-file token-file
 kubectl apply -f ds1.yaml --access-token-file token-file
