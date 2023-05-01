02c5b9000ef0f0e9bb86cae9d4dafb4f72266202
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-05-01 06:02:47 +0000

    hopefully

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-# for the sake of changing - 20
+# for the sake of changing - 30
 gcloud components install gke-gcloud-auth-plugin && \
 kubectl apply -n default --token "$service_account_key" -f ds1.yaml
