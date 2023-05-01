73ee3decbc708f551d35a23651a15ea08db6629b
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-05-01 02:43:47 +0000

    new commit

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
-
+# for the sake of changing - 1
 gcloud config set project kube-project-385314
-kubectl apply -n default -s $1 -f ds1.yaml --token=$2 
+kubectl apply -n default -s $1 -f ds1.yaml --token=$2  
