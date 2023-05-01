a8655a8b9ea2f493f5fd12f602f350668c26681e
pnemani0193 <pardhasaradhisastry.nemani@valpo.edu>
2023-05-01 05:55:19 +0000

    with the plugin

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 
 # for the sake of changing - 10
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin && \
 kubectl apply -n default --token "$service_account_key" -f ds1.yaml
